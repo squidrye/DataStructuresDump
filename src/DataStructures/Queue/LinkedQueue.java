dac0b976d27de4f28a07d3d479241afc11de44e1
squidrye <ancientphoebus@gmail.com>
2022-02-03 04:15:04 +0530

    queue implementation

---
+++ src/DataStructures/Queue/LinkedQueue.java
@@ -0,0 +1,40 @@
+package DataStructures.Queue;
+
+import DataStructures.LinkedList.DoublyLinkedList;
+import java.util.Iterator;
+public class LinkedQueue<E> implements Queue<E>, Iterable<E>{
+    DoublyLinkedList<E> list=new DoublyLinkedList<>();
+    LinkedQueue(){};
+    LinkedQueue(E elem){
+        offer(elem);
+    }
+    public int size(){
+        return list.size();
+    }
+    public void offer(E elem){
+        list.addLast(elem);
+    }
+    public boolean isEmpty(){
+        return size()==0;
+    }
+    public E poll(){
+        if(isEmpty()){
+            throw new RuntimeException("Queue is empty");
+        }else{
+            return list.removeFirst();
+        }
+    }
+    public E peak(){
+        if(isEmpty()){
+            throw new RuntimeException("Queue is empty");
+        }else{
+            return list.peekFirst();
+        }
+    }
+    public Iterator<E> iterator(){
+        return list.iterator();
+    }
+    public boolean isPresent(E elem){
+        return list.contains(elem);
+    }
+}
---
+++ src/DataStructures/Queue/Queue.java
@@ -0,0 +1,10 @@
+package DataStructures.Queue;
+
+public interface Queue<E>{
+    public int size();
+    public boolean isEmpty();
+    public void offer(E elem);//push
+    public E poll();//pop   
+    public boolean isPresent(E obj);
+    public E peak();
+}
