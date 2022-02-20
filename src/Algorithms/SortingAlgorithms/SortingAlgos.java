3e8ae3672e27c0ee412a5e7bcc75f0991213a7b1
squidrye <ancientphoebus@gmail.com>
2022-02-20 20:21:15 +0530

    add quick and merge sort

--- src/Algorithms/SortingAlgorithms/SortingAlgos.java
+++ src/Algorithms/SortingAlgorithms/SortingAlgos.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SortingAlgos {
     public static void main(String[] args) {
         int [] arr = {4,3,2,5,1};
-        insertion(arr);
+        quick(arr,0, arr.length - 1 );
         System.out.println(Arrays.toString(arr));
     }
     static void bubble(int [] arr){
@@ -50,5 +50,114 @@ public class SortingAlgos {
                 }else break;
             }
         }
+    }
+
+
+    static int[] mergeSort(int [] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+        int mid = ( arr.length)/2;
+        int [] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int [] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+    static int[] merge(int [] left, int [] right){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int [] mix = new int[left.length + right.length];
+        while(i<left.length && j<right.length){
+            if(left[i] < right[j]){
+                mix[k] = left[i];
+                i++;
+            }else{
+                mix[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < left.length){
+            mix[k] = left[i];
+            k++;
+            i++;
+        }
+        while(j < right.length){
+            mix[k] = right[j];
+            k++;
+            j++;
+        }
+        return mix;
+    }
+
+    static void mergeSortInPlace(int [] arr, int start, int end){
+        if(end - start == 1){
+            return;
+        }
+        int mid = (start + end)/2;
+        mergeSortInPlace(arr, start, mid);
+        mergeSortInPlace(arr, mid, end);
+
+        mergeInPlace(arr, start, mid, end);
+    }
+    static void mergeInPlace(int [] arr, int start , int mid, int end){
+        int [] mix = new int[end - start];
+
+        int i = start;
+        int j = mid;
+        int k = 0;
+        while(i < mid && j < end){
+            if(arr[i] < arr[j]){
+                mix[k] = arr[i];
+                i++;
+            }else{
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        //it may be possible that one of the arrays isn't completely traversed
+        while(i < mid){
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j < end){
+            mix[k] = arr[j];
+            k++;
+            j++;
+        }
+        for(int l=0;l<mix.length;l++){
+            arr[start + l] = mix[l];
+        }
+    }
+
+    static void quick(int [] arr, int low, int high){
+        if(low >= high){
+            return;
+        }
+        int start = low;
+        int end = high;
+        int mid = (start + end)/2;
+        int pivot = arr[mid];
+
+        while(start <= end){
+            while(arr[start] < pivot){
+                start++;
+            }
+            while(arr[end] > pivot){
+                end--;
+            }
+            if(start <= end){
+                int temp = arr[end];
+                arr[end] = arr[start];
+                arr[start] = temp;
+                start++;
+                end--;
+            }
+        }
+        quick(arr, low, end);
+        quick(arr, start, high);
     }
  }
